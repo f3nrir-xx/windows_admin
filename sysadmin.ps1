f88ca5f0e4c924aba3023911caf7f039b45febe9
F3NRIR_XX <64734975+f3nrir-xx@users.noreply.github.com>
2022-10-20 20:58:59 +0300

    Add files via upload

---
+++ sysadmin.ps1
@@ -0,0 +1,156 @@
+﻿#list of Computers Name
+$computers=@("4T-LB-PC2",
+             "4T-LB-LP4",
+             "4T-PC7",
+             "4T-LB-PC14",
+             "4T-LB-PC18")
+Function Get-MonitorSN{
+#List of Manufacture Codes and their respective full names
+$ManufacturerHash = @{ 
+    "AAC" =	"AcerView";
+    "ACR" = "Acer";
+    "AOC" = "AOC";
+    "AIC" = "AG Neovo";
+    "APP" = "Apple Computer";
+    "AST" = "AST Research";
+    "AUO" = "Asus";
+    "BNQ" = "BenQ";
+    "CMO" = "Acer";
+    "CPL" = "Compal";
+    "CPQ" = "Compaq";
+    "CPT" = "Chunghwa Pciture Tubes, Ltd.";
+    "CTX" = "CTX";
+    "DEC" = "DEC";
+    "DEL" = "Dell";
+    "DPC" = "Delta";
+    "DWE" = "Daewoo";
+    "EIZ" = "EIZO";
+    "ELS" = "ELSA";
+    "ENC" = "EIZO";
+    "EPI" = "Envision";
+    "FCM" = "Funai";
+    "FUJ" = "Fujitsu";
+    "FUS" = "Fujitsu-Siemens";
+    "GSM" = "LG Electronics";
+    "GWY" = "Gateway 2000";
+    "HEI" = "Hyundai";
+    "HIT" = "Hyundai";
+    "HSL" = "Hansol";
+    "HTC" = "Hitachi/Nissei";
+    "HWP" = "HP";
+    "IBM" = "IBM";
+    "ICL" = "Fujitsu ICL";
+    "IVM" = "Iiyama";
+    "KDS" = "Korea Data Systems";
+    "LEN" = "Lenovo";
+    "LGD" = "Asus";
+    "LPL" = "Fujitsu";
+    "MAX" = "Belinea"; 
+    "MEI" = "Panasonic";
+    "MEL" = "Mitsubishi Electronics";
+    "MS_" = "Panasonic";
+    "NAN" = "Nanao";
+    "NEC" = "NEC";
+    "NOK" = "Nokia Data";
+    "NVD" = "Fujitsu";
+    "OPT" = "Optoma";
+    "PHL" = "Philips";
+    "REL" = "Relisys";
+    "SAN" = "Samsung";
+    "SAM" = "Samsung";
+    "SBI" = "Smarttech";
+    "SGI" = "SGI";
+    "SNY" = "Sony";
+    "SRC" = "Shamrock";
+    "SUN" = "Sun Microsystems";
+    "SEC" = "Hewlett-Packard";
+    "TAT" = "Tatung";
+    "TOS" = "Toshiba";
+    "TSB" = "Toshiba";
+    "VSC" = "ViewSonic";
+    "ZCM" = "Zenith";
+    "UNK" = "Unknown";
+    "_YV" = "Fujitsu";
+      }
+
+
+
+#Grabs the Monitor objects from WMI
+$Monitors = Get-WmiObject -Namespace "root\WMI" -Class "WMIMonitorID" -ComputerName $computers -ErrorAction SilentlyContinue
+    
+#Creates an empty array to hold the data
+$Monitor_Array = @()
+
+#Takes each monitor object found and runs the following code:
+    ForEach ($Monitor in $Monitors) {
+      
+      #Grabs respective data and converts it from ASCII encoding and removes any trailing ASCII null values
+      If ([System.Text.Encoding]::ASCII.GetString($Monitor.UserFriendlyName) -ne $null) {
+        $Mon_Model = ([System.Text.Encoding]::ASCII.GetString($Monitor.UserFriendlyName)).Replace("$([char]0x0000)","")
+      } else {
+        $Mon_Model = $null
+      }
+      ####
+
+      $Mon_Serial_Number = ([System.Text.Encoding]::ASCII.GetString($Monitor.SerialNumberID)).Replace("$([char]0x0000)","")
+      $Mon_Attached_Computer = ($Monitor.PSComputerName).Replace("$([char]0x0000)","")
+      $Mon_Manufacturer = ([System.Text.Encoding]::ASCII.GetString($Monitor.ManufacturerName)).Replace("$([char]0x0000)","")
+      
+      #Remove The Manufacturer Code and replace it with the full name
+      $Mon_Manufacturer_Friendly = $ManufacturerHash.$Mon_Manufacturer
+      If ($Mon_Manufacturer_Friendly -eq $null) {
+        $Mon_Manufacturer_Friendly = $Mon_Manufacturer
+      }
+      
+      #Creates a custom monitor object 
+      $Monitor_Obj = [PSCustomObject]@{
+        Manufacturer     = $Mon_Manufacturer_Friendly
+        Model            = $Mon_Model
+        SerialNumber     = $Mon_Serial_Number
+        AttachedComputer = $Mon_Attached_Computer
+      }
+      
+      #Appends the object to the array
+      $Monitor_Array += $Monitor_Obj
+      
+
+    } #End ForEach Monitor
+  
+#Outputs the Array
+$Monitor_Array
+
+
+}
+Function Get-LoggedUsers{
+   Get-WmiObject –ComputerName $computers –Class Win32_ComputerSystem | Select-Object Username, Name, Model
+}
+
+
+
+Function Menu{
+    Write-Host @"
+    +===============================================+
+    |  System Administrator Automation Tool         |
+    +===============================================+
+    |                                               |
+    |    1) Monitor Serial Number                   |
+    |    2) Logged In Users                         |
+    |                                               |
+    |                                               |
+    +===============================================+
+"@
+
+$Read = Read-Host "OPTION"
+Switch ($Read)
+{
+1 {
+$option1=Get-MonitorSN
+$option1
+}
+2 { $option2=Get-LoggedUsers
+    $option2
+  }
+}
+}
+
+Menu
